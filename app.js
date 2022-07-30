92aec7a5072470c29121c70c83f5bf9c329baa79
ferdous <hassanferdous101@gmail.com>
2022-07-30 16:28:55 +0600

    setup deployment

--- app.js
+++ app.js
@@ -50,6 +50,13 @@ app.use((err, req, res, next) => {
 //server
 let PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	});
+}
+
 app.listen(PORT, () => {
 	console.log(`listening on the port ${PORT}`);
 });
