ec99dfd9161b789ddd214ed705134d07b27d86b6
ferdous <hassanferdous101@gmail.com>
2022-07-30 19:07:05 +0600

    change basequery

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -4,7 +4,7 @@ import { REHYDRATE } from 'redux-persist';
 const hexaApi = createApi({
 	reducerPath: 'hexaApi',
 	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://localhost:5000/api/',
+		baseUrl: 'https://mern-hexa.herokuapp.com/api/',
 		prepareHeaders: (headers, { getState }) => {
 			const token = getState().user.token;
 			if (token) {
