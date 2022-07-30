10b0d6df521618abfed89d4a4f21a2cdd1e61278
ferdous <hassanferdous101@gmail.com>
2022-07-30 14:18:18 +0600

    remove unused line

--- client/src/components/login/loginModal.js
+++ client/src/components/login/loginModal.js
@@ -2,11 +2,9 @@ import { Link } from 'react-router-dom';
 import useModal from '../../hooks/modal';
 import { useCallback, useState } from 'react';
 import { postData } from '../../utilities/utils';
-import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const LoginModal = ({ hideModal }) => {
-	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	//state
 	const { toggle } = useModal();
